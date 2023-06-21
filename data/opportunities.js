1f0d68c634c3a84f0923b0e706af9d10f010efb4
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2023-06-21 20:52:30 +0530

    Update opportunities.js

--- data/opportunities.js
+++ data/opportunities.js
@@ -1,37 +1,82 @@
 const data = [
   {
-    title: "Lead Partner - Ventures",
+    title: "Lead, Entrepreneurial culture",
     description:
-      "The Lead Partner would be in-charge of the portfolio and the seamless execution of their activities as a part of CoCreate.",
+      "Creating the right environment in motivating bright youngsters in joining hands to build amazing innovations for the world",
     image: "/LeadPartner.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Lead%20%20-%20culture%20and%20entrepreneurship?type=2&title=Lead%20-%20Culture%20and%20entrepreneurship",
     height: 110,
     width: 42,
+  },
+  {
+    title: "Lead, Innovations & Solutions",
+    description:
+      "Convert innovative technologies into product and commercial propositions",
+    image: "/CyberSecurity.svg",
+    link: "https://assertify.me/jobs/CoCreate/Solutions%20Lead?type=2&title=Solutions%20Lead",
+    height: 70,
+    width: 70,
+  },
+  {
+    title: "Lead, Entrepreneurial Communities",
+    description:
+      "Building communities of innovators and entrepreneurs",
+    image: "/CyberSecurity.svg",
+    link: "https://assertify.me/jobs/CoCreate/Community%20manager?type=2&title=Community%20Manager",
+    height: 70,
+    width: 70,
+  },
+  {
+    title: "Lead, Digital Growth Hacking",
+    description:
+      "Establishing an end-to-end leads process and guiding the implementation and execution across multiple social media platforms",
+    image: "/CyberSecurity.svg",
+    link: "https://assertify.me/jobs/CoCreate/Digital%20Marketing?type=2&title=Digital%20Marketing%20Officer",
+    height: 70,
+    width: 70,
+  },
+  {
+    title: "Lead, Content & Social Engagements",
+    description:
+      "Create Viral and rich content pieces and strategies",
+    image: "/CyberSecurity.svg",
+    link: "https://assertify.me/jobs/CoCreate/Content%20Writing%20-%20General?type=2&title=Content%20Writer",
+    height: 70,
+    width: 70,
+  },
+  {
+    title: "Investor Outreach Head",
+    description:
+      "Connecting with Investors, VCs, Angel Networks and Funds to invest with and in CoCreate",
+    image: "/CyberSecurity.svg",
+    link: "https://assertify.me/jobs/CoCreate/Investor%20Outreach%20Head%20for%20venture%20studios?type=2&title=Investor%20Outreach%20Head",
+    height: 70,
+    width: 70,
   },
   {
     title: "UI/UX Designer",
     description:
       "UI/UX Designer responsibilities include gathering user requirements, designing graphic elements, and building interface components.",
     image: "/Designer.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Design%20UI_UX?type=2&title=UI/UX%20Designer",
     height: 73,
     width: 73,
   },
   {
-    title: "React Frontend Developer",
+    title: "Frontend Web Developer",
     description:
       "Working in tandem with the UI/UX team to develop new features using React.js for the web platform.",
     image: "/Frontend.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Front%20End%20Web%20Development?type=2&title=Front%20End%20Web%20Developer",
     height: 92,
     width: 64,
   },
   {
-    title: "Backend Developer",
+    title: "Backend Web Developer",
     description:
       "Work closely with the product head and UX designers to understand the product vision and design philosophy.",
     image: "/Backend.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Backend%20Developer?type=2&title=Backend%20Web%20Developer",
     height: 61,
     width: 72,
   },
@@ -40,25 +85,16 @@ const data = [
     description:
       "Developing end-to-end web applications, Creating rapid prototypes and upgrading them to full-scale applications by working collaboratively with the design and development team",
     image: "/FullStack.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Full%20Stack%20Developer?type=2&title=Full%20Stack%20Developer",
     height: 66,
     width: 73,
-  },
-  {
-    title: "Cyber Security and Network Engineers",
-    description:
-      "Looking for dedicated interns who can work in various cybersecurity areas and enhance their experience and relevant skills in cybersecurity.",
-    image: "/CyberSecurity.svg",
-    link: "",
-    height: 70,
-    width: 70,
   },
   {
     title: "Content Writer",
     description:
       "If you know and love content creation like we at PVL love innovation, let’s get talking.",
     image: "/ContentWriter.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Content%20Writing%20-%20General?type=2&title=Content%20Writer",
     height: 70,
     width: 70,
   },
@@ -67,7 +103,7 @@ const data = [
     description:
       "Accomplishing crucial coverage for portfolio companies and working to enhance media presence of CoCreate in an innovative manner.",
     image: "/PR.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/PR%20Outreach?type=2&title=PR%20and%20Outreach%20Executive",
     height: 70,
     width: 70,
   },
@@ -76,7 +112,7 @@ const data = [
     description:
       "Establishing an end-to-end leads process and guiding the implementation and execution across multiple social media platforms.",
     image: "/DigitalMarketing.svg",
-    link: "",
+    link: "https://assertify.me/jobs/CoCreate/Digital%20Marketing?type=2&title=Digital%20Marketing%20Officer",
     height: 70,
     width: 70,
   },
