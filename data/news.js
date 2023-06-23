93a8c8968fc12d1ad4bbabeddcc5517566f9a237
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2023-06-23 18:01:29 +0530

    linked images

--- data/news.js
+++ data/news.js
@@ -3,36 +3,42 @@ const data = [
     heading:
       "This deep-tech-focused micro fund helps student-led startups build scalable businesses",
     date: "June 21, 2021",
+    image: "/deeptech_focused.webp",
     link: "https://yourstory.com/2021/06/pesu-venture-labs-deep-tech-micr-fund-studentled-startups/amp",
   },
   {
     heading:
       "CoCreate Ventures acquires Startupreneur's platform",
     date: "Aug 2, 2022",
+    image: "/Cocreate_acq_startupreneur.webp",
     link: "https://techherald.in/emerging-business/startup/cocreate-ventures-acquires-startupreneurs-platform/",
   },
   {
     heading:
       "PESU Venture Labs to invest in 50 startups",
     date: "June 23, 2022",
+    image: "/PVL_50invest.webp",
     link: "https://yourstory.com/2022/06/startup-news-and-updates-daily-roundup-june-23-2022/amp",
   },
   {
     heading:
       "Seminarroom bridges industry skill gap...",
     date: "July 29, 2022",
+    image: "/SR_bridges.webp",
     link: "https://www.thehindu.com/news/national/karnataka/introducing-new-age-teachers-to-address-an-age-old-problem/article36297249.ece",
   },
   {
     heading:
       "Incubation, Entrepreneurship and Seed Funding",
     date: "Aug 1, 2022",
+    image: "/Inc_Entr_Seed.webp",
     link: "https://youtu.be/JgnkhtYvGhk",
   },
   {
     heading:
       "Introducing new-age teachers to address...",
     date: "Sept 06, 2021",
+    image: "/newage.webp",
     link: "https://www.thehindu.com/news/national/karnataka/introducing-new-age-teachers-to-address-an-age-old-problem/article36297249.ece",
   },
 ];
