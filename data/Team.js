d08c65c0a7f30f5a661160d51a632fc1fa11042e
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2023-06-28 15:38:20 +0530

    updated members
    removed Madhu

--- data/Team.js
+++ data/Team.js
@@ -2,7 +2,7 @@ const data = [
   { src: "/SureshNarsimha.png", name: "Suresh Narsimha", title: "MD" },
   { src: "/ChetanShirnali.png", name: "Chetan Shirnali" },
   { src: "/AnilJ.png", name: "Anil J" },
-  { src: "/MadhuV.png", name: "Madhu V" },
+  //{ src: "/MadhuV.png", name: "Madhu V" },
   { src: "/KiritiKapavari.png", name: "Kiriti Kapavari" },
   { src: "/SantoshBishnoi.png", name: "Santosh Bishnoi" },
 ];
