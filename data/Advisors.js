b4557896118169b65bf8bb0d5c5df87af40048fd
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2023-07-11 11:40:34 +0530

    Updated Advisors

--- data/Advisors.js
+++ data/Advisors.js
@@ -1,6 +1,8 @@
 const data = [
   { src: "/RavindraKrishnappa.jpg", name: "Ravindra Krishnappa" },
   { src: "/PrashanthPrakash.jpg", name: "Prashanth Prakash" },
+  { src: "/PavanK.jpg", name: "Pavan Krishnamurthy" },
+  { src: "/nopic.png", name: "Arun Seetharam" },
 ];
 
 export default data;
