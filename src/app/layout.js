967a1f7abdb14f3d469d79d1222ba0440247bf55
Mr.Nayra <aryanedu5786@gmail.com>
2023-08-03 12:43:54 +0530

    cocreate favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
   title: "CoCreate",
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
   description:
     "PESU Venture Labs is a deep-tech-focused micro fund that operates in the pre-prototype or pre-ideation phase and aims to help industry professionals, student entrepreneurs, and academicians build scalable startups.",
 };
@@ -11,5 +14,5 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
