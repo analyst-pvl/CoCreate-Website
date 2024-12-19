61c8e2f53720b4fa3f2c7730172d317e53312521
analyst-pvl <86413677+analyst-pvl@users.noreply.github.com>
2024-12-19 15:42:08 +0530

    Fix for image sizes

--- src/components/LandingPage/Solutions.js
+++ src/components/LandingPage/Solutions.js
@@ -7,11 +7,11 @@ const Solutions = () => {
     <div className="flex justify-center bg-[#1F1F1F] text-[#F0F0F0]">
       <div className="container py-20 relative">
         <div className="flex items-center justify-center gap-x-[10%]">
-          <div className="border-2 w-[10%] border-black" />
+          <div className="border-2 w-[10%] border-white" />
           <h2 className="text-4xl lg:text-5xl text-center font-medium">
             Solutions & Frameworks
           </h2>
-          <div className="border-2 w-[10%] border-black" />
+          <div className="border-2 w-[10%] border-white" />
         </div>
         <div className="grid text-black grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-20 gap-10 ">
           {data.map((item, index) => (
